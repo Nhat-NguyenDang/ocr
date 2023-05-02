32930bb7c2364a0b20f6eeb7f570e5fb9fc2250b
Nhat Nguyen Dang <105844648+Nhat-NguyenDang@users.noreply.github.com>
2023-05-02 16:47:07 +0900

    add dilate and resize function

--- add_column_space.py
+++ add_column_space.py
@@ -65,9 +65,12 @@ def myFunc(x):
 
 x_coordinate.sort(key=myFunc)
 print(img2.shape)
+
 def insert_space(img, space):
 	space_matrix = np.full((space, 1), 240)
-	for index, coordinate in enumerate(x_coordinate[2:-15]):
+	column = x_coordinate[2:-15]
+	print(column)
+	for index, coordinate in enumerate(column):
 		# for i in range (1, space+1):
 		# 	img = np.insert(img, coordinate[0]+space*a, 240, axis=1)
 		img = np.insert(img, [coordinate[0]+space*index], space_matrix, axis=1)
@@ -78,6 +81,35 @@ img2 = insert_space(img2, space=space)
 print(img2.shape)
 cv2.imshow('after', img2)
 cv2.imwrite(working_dir+f'space_added_{space}.jpg', img2)
+
+# Cut off white space and resize to 1024 x 768 (recommended size)
+# img = cv2.imread('temp/test_imgs/space_added_30.jpg') # Read in the image and convert to grayscale
+img3 = img2[20:-20,20:-20] # Perform pre-cropping
+gray = cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY)
+gray = 255*(gray < 128).astype(np.uint8) # To invert the text to white
+gray = cv2.morphologyEx(gray, cv2.MORPH_OPEN, np.ones((2, 2), dtype=np.uint8)) # Perform noise filtering
+coords = cv2.findNonZero(gray) # Find all non-zero points (text)
+x, y, w, h = cv2.boundingRect(coords) # Find minimum spanning bounding box
+rect = img3[y-50:y+h+50, x-50:x+w+50] # Crop the image - note we do this on the original image
+
+cv2.imshow("Cropped", rect) # Show it
+cv2.imwrite("rect.png", rect) # Save the image
+
+resized_image = cv2.resize(rect, (1024, 768), cv2.INTER_LANCZOS4)
+cv2.imwrite("resized_image.png", resized_image)
+cv2.imshow("Resized", resized_image)
+
+# Dilate
+def thick_font(image):
+    import numpy as np
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2,2),np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return (image)
+
+dilated_image = thick_font(resized_image)
+cv2.imwrite("test.jpg", dilated_image)
 # Exiting the window if 'q' is pressed on the keyboard.
 if cv2.waitKey(0) & 0xFF == ord('q'):
 	cv2.destroyAllWindows()
